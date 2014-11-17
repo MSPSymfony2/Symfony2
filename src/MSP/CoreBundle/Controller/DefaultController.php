3734926dbdd256ea3310b2ee5ad3c79842c4004c
tuxi <tuxi@tuxi.com>
2014-11-17 09:41:17 -0800

    laborator_1

--- src/MSP/CoreBundle/Controller/DefaultController.php
+++ src/MSP/CoreBundle/Controller/DefaultController.php
@@ -6,14 +6,62 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+/**
+ * @Route("/app")
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * Comentariu
+     *
+     * @Route("/post/{id}", name="hello", defaults={"id" = "1"})
      * @Template()
      */
-    public function indexAction($name)
+    public function helloAction($id)
     {
-        return array('name' => $name);
+        // msp_core_default_hello
+        return [
+            'id' => $id
+        ];
+    }
+
+    /**
+     * @Route("/contact-us", name="contact")
+     * @Template()
+     */
+    public function contactAction()
+    {
+        $email = 'contact-us@email.com';
+
+        return [
+            'message' => 'Hello',
+            'email' => $email
+        ];
+    }
+
+    /**
+     * Comentariu
+     *
+     * @Route("/if/{var}", name="if")
+     * @Template("MSPCoreBundle:Default:if.html.twig")
+     */
+    public function ifAction($var)
+    {
+        return [
+            'var' => $var
+        ];
+    }
+
+    /**
+     * @Route("/for", name="for")
+     * @Template()
+     */
+    public function forAction()
+    {
+        $users = ['USer1'];
+
+        return [
+            'utilizatori' => $users
+        ];
     }
 }
